a0b2f8531de98be71abddd15281a59f6b4b8aaed
Nguyen N Bao Tram <tramnguyenngocbao2000@gmail.com>
2024-02-21 22:14:04 +0700

    first update version

---
+++ assets/javascript/main.js
@@ -0,0 +1,7 @@
+// // Get the modal
+// var modal = document.getElementById("modal");
+// // Get the events that opens the modal
+// var register = document.getElementsByClassName("header-navbar__icon--register");
+// // When the user hover on the element, open the modal
+
+// modal.style.display = "flex";
